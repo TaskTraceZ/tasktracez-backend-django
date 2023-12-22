78cdf07a7d577892235abef9dee01c0a1e757240
Tirthya Kamal dasgupta <dasguptatirthyakamal@gmail.com>
2023-12-22 09:26:48 +0530

    feat: The related task's title is now provided while fetching the list of the task instances.

--- task_instance/serializers.py
+++ task_instance/serializers.py
@@ -4,6 +4,8 @@ from task_instance.models import TaskInstance
 
 
 class TaskInstanceSerializer(serializers.ModelSerializer):
+    title = serializers.StringRelatedField(source='task.title', read_only=True)
+
     class Meta:
         model = TaskInstance
         fields = '__all__'
